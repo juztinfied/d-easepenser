9f75fb175e220b82916c5f5b1ee4a1f0c4b2a1bd
juztinfied <z_d_143@hotmail.com>
2018-03-23 01:35:53 +0800

    completed the UI for d'easepenser bot i.e. upload recipes, view recipes, step-by-step prompting for baking

--- deasepenserUI.py
+++ deasepenserUI.py
@@ -36,18 +36,25 @@ worksheet = sheet.get_worksheet(0)
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 #BOT_TOKEN = '476170496:AAEp4-B-N96lVb4GZOhhREjNhVIs9x0hpbo' #for tin_bot
 BOT_TOKEN = '529982566:AAEhBusHHmFZ6Tq0zVUCg8KBOMgbdh1Z16c' #for deasepenser
+
+#for uploading new recipes
 recipeName = 'empty'
 ingredientList = 'empty'
 recipeInstructions = 'empty'
 confirm_vector = {'rname': False, 'ingredientList': False, 'recipeInstructions': False}
 
+#for baking process
+selectedRecipe = list()
+stepcount = -1;
+splitInstructions = list()
+
 #states
-USER_CHOICE, RECIPE_NAME, INPUT_TYPE, CONFIRMATION, MAINMENU = range(5)
+USER_CHOICE, RECIPE_NAME, INPUT_TYPE, CONFIRMATION, MAINMENU, BAKE_OR_BACK, RECIPE_CHOICE, NEXT_STEP = range(8)
 
-
+#bot functions
 def start(bot, update):
     update.message.reply_text("In start function") 
-    reply_keyboard = [['Upload Recipe', 'View Recipes'],['BAKE']]
+    reply_keyboard = [['Upload Recipe', 'View Recipes']]
 
     update.message.reply_text(
         'How can I help you?',
@@ -57,10 +64,45 @@ def start(bot, update):
 
 def view_recipes(bot,update):
     update.message.reply_text("In view_recipe function")
+    recipeCatalogue = worksheet.col_values(1)
+    count = 1;
+    outputmessage = 'Recipes uploaded:\n'
+    update.message.reply_text("Showing recipe catalogue:")
+    
+    for recipe in recipeCatalogue:
+        if (recipe == 'Recipe_name'):
+            continue
+        else:
+            outputmessage += (str(count) + ". " + recipe + "\n")
+            count += 1
+    update.message.reply_text(outputmessage)
+    count = 1;
 
+    bot.send_message(chat_id=update.message.chat_id, text="Select recipe number you wish to view")
+
+    return RECIPE_CHOICE
+
+def recipe_selected(bot,update):
+    global selectedRecipe
+    selectedRecipeNo = int(update.message.text) + 1
+    selectedRecipe = worksheet.row_values(selectedRecipeNo)
+    #ingredList = worksheet.cell(selectedRecipeNo,2).value
+    #instructions = worksheet.cell(selectedRecipeNo,3).value
+    bot.send_message(chat_id=update.message.chat_id, text="Ingredient list for " + selectedRecipe[0] + ":\n" + selectedRecipe[1])
+    bot.send_message(chat_id=update.message.chat_id, text="Instructions for " + selectedRecipe[0] + ":\n" + selectedRecipe[2])
+
+    reply_keyboard = [['BAKE', 'MAIN MENU']]
+
+    update.message.reply_text(
+        'What would you like to do now?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return BAKE_OR_BACK
+    
 def enter_recipe_name(bot,update):
     update.message.reply_text("In recipe_name function") 
     bot.send_message(chat_id=update.message.chat_id, text="Name of recipe?")
+    
     return RECIPE_NAME
 
 def enter_ingredients(bot,update):
@@ -103,7 +145,8 @@ def text_to_text(bot,update):
 def image_to_text(bot,update):
     global recipeInstructions
     global ingredientList
-    
+
+    tesseract_results = 'still empty'
     user = update.message.from_user
     update.message.reply_text("in image_to_text function") 
     #fetching photo id from message
@@ -142,11 +185,10 @@ def image_to_text(bot,update):
         tesseract_results = pytesseract.image_to_string(img)
         bot.send_message(chat_id=update.message.chat_id, text = tesseract_results)
 
-    ingredientList = tesseract_results
     reply_keyboard = [['Yes', 'No']]
 
-    if (confirm_vector['ingredientList'] == False): #if ingredientList not confirmed yet
-        ingredientList = update.message.text #fill up ingredientList
+    if (ingredientList == 'empty' or confirm_vector['ingredientList'] == False): #if ingredientList not filled yet or not confirmed yet
+        ingredientList = tesseract_results #fill up ingredientList with user input
         bot.send_message(chat_id=update.message.chat_id, text = 'Ingredient list: ')
         bot.send_message(chat_id=update.message.chat_id, text = ingredientList)
         bot.send_message(chat_id=update.message.chat_id,
@@ -154,8 +196,8 @@ def image_to_text(bot,update):
                          reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
                          )
     
-    elif (confirm_vector['recipeInstructions'] == False): #if recipeInstructions not filled yet
-        recipeInstructions = update.message.text
+    elif (recipeInstructions == 'empty' or confirm_vector['recipeInstructions'] == False): #if recipeInstructions not filled yet or not confirmed
+        recipeInstructions = tesseract_results
         bot.send_message(chat_id=update.message.chat_id, text = 'Recipe Instructions: ')
         bot.send_message(chat_id=update.message.chat_id, text = recipeInstructions)
         bot.send_message(chat_id=update.message.chat_id,
@@ -199,7 +241,6 @@ def enter_instructions(bot,update):
     else:
         bot.send_message(chat_id=update.message.chat_id, text = 'Something is wrong somewhere...')
         return ConversationHandler.END
-        
 
 def upload_data(bot,update):
     update.message.reply_text("In upload_data function")
@@ -218,8 +259,8 @@ def upload_data(bot,update):
                      'Instructions:'+rowData[2]
                     )
     
-    reply_keyboard = [['Terminate', 'Main Menu']]
-    bot.send_message(chat_id=update.message.chat_id, text = 'Where do you want to go now?',
+    reply_keyboard = [['TERMINATE', 'MAIN MENU']]
+    bot.send_message(chat_id=update.message.chat_id, text = 'What would you like to do now?',
                      reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
                      )
     return MAINMENU
@@ -230,16 +271,58 @@ def cancel(bot, update):
     update.message.reply_text('Exiting sequence... (Type /start to reset sequence)', reply_markup=ReplyKeyboardRemove())
     return ConversationHandler.END
 
-
 def error(bot, update, error):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, error)
+
+def main_menu_redirect(bot,update):
+    bot.send_message(chat_id=update.message.chat_id, text = 'Returning to main menu...')
+    start(bot,update)
+
+def terminate_bot(bot,update):
+    bot.send_message(chat_id=update.message.chat_id, text = 'Shutting down d-easepenser bot...')
+    cancel(bot,update)
     
 def bake(bot,update):
+    global stepcount
+    global splitInstructions
+    global selectedRecipe
+    
     update.message.reply_text("In bake function")
     logger.info("User selected BAKE")
-
     
+    if(stepcount == -1): #if user is entering this function for the first time
+        bot.send_message(chat_id=update.message.chat_id, text = 'You have chosen to start baking process for ' + selectedRecipe[0])
+        splitInstructions = selectedRecipe[2].split("\n")
+        stepcount = 0
+        reply_keyboard = [['NEXT STEP']]
+        bot.send_message(chat_id=update.message.chat_id, text = splitInstructions[stepcount],
+                         reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+                         )
+        stepcount += 1
+        return NEXT_STEP
+        
+    elif (stepcount == len(splitInstructions)): #if user has done all instructions for the baking sequence
+        reply_keyboard = [['TERMINATE'],['MAIN MENU']]
+        bot.send_message(chat_id=update.message.chat_id, text = 'End of baking process for ' + selectedRecipe[0])
+        selectedRecipe = list() #setting the global variables back to default value for next baking process
+        stepcount = -1
+        splitInstructions = list()
+
+        reply_keyboard = [['TERMINATE', 'MAIN MENU']]
+        bot.send_message(chat_id=update.message.chat_id, text = 'What would you like to do now?',
+                         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+                         )
+        return MAINMENU
+                          
+    else: #if user is midway in a baking sequencde
+        reply_keyboard = [['NEXT STEP']]
+        bot.send_message(chat_id=update.message.chat_id, text = splitInstructions[stepcount],
+                         reply_markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+                         )
+        stepcount += 1
+        return NEXT_STEP
+
 def main():
     print("D'easepenser is running...")
     # Create the EventHandler and pass it your bot's token.
@@ -257,11 +340,14 @@ def main():
                                        enter_recipe_name),
                           RegexHandler('^(View Recipes)$',
                                        view_recipes),
-                          RegexHandler('^(BAKE)$',
-                                        bake)
                           ],
             
-            RECIPE_NAME: [MessageHandler(Filters.text, enter_ingredients)],
+            RECIPE_CHOICE: [RegexHandler('^[-+]?[0-9]+$',recipe_selected),
+                            MessageHandler(Filters.text, main_menu_redirect),
+                            ],
+            
+            RECIPE_NAME: [MessageHandler(Filters.text, enter_ingredients)
+                          ],
 
             INPUT_TYPE: [MessageHandler(Filters.photo, image_to_text),
                          MessageHandler(Filters.text, text_to_text),
@@ -273,10 +359,17 @@ def main():
                            MessageHandler(Filters.text,text_to_text),
                            ],
 
-            MAINMENU: [RegexHandler('^(Terminate)$', view_recipes),
-                       RegexHandler('^(Main Menu)$', enter_recipe_name),
-                       MessageHandler(Filters.text,view_recipes),
-                       ]
+            MAINMENU: [RegexHandler('^(Terminate)$', terminate_bot),
+                       RegexHandler('^(Main Menu)$', main_menu_redirect),
+                       ],
+            
+            BAKE_OR_BACK: [RegexHandler('^(BAKE)$', bake),
+                           RegexHandler('^(MAIN MENU)$', main_menu_redirect),
+                           ],
+
+            NEXT_STEP: [MessageHandler(Filters.text,bake),
+                        ],
+            
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
@@ -298,4 +391,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-    print("i am in the if main thing")
